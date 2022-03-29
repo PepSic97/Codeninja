b445277863284a83b193e20ba7ea56ee93521a4a
Giuseppe Sica <92546622+PepSic97@users.noreply.github.com>
2022-03-29 11:58:01 +0200

    Add files via upload

---
+++ Types/Types.swift
@@ -0,0 +1,13 @@
+//
+//  Types.swift
+//  CodeNinja
+//
+//  Created by Giuseppe Sica on 21/03/22.
+//
+
+import UIKit
+
+public let screenWidth: CGFloat = 1792.0
+public let screenHeight: CGFloat = 828.0
+
+let appDl = UIApplication.shared.delegate as! AppDelegate
